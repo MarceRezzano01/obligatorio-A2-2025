769ffe707d9c3095338e5766eefa1bdfdfda8468
MarceloNicolas RezzanoDiaz <marcelo.rezzanodiaz@ukg.com>
2025-03-29 20:53:19 -0300

    ej1 init

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,10 +2,30 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/HashAbierto.cpp"
+#include "tads/ListImp.cpp"
 
 using namespace std;
 
+int hash3(string k){
+    int h = 0;
+  for (int i = 0; i < k.length(); i++)
+    h = 31 * h + int(k[i]);
+  return h;
+
+}
+
 int main()
 {
+
+ HashAbierto<string, string> *mihash = new HashAbierto <string,string>(10,hash3);
+
+ //HashAbierto <string,int> *mihash = new HashAbierto <string,int>(10,hash3);
+mihash->insert("pepe@pepe","pepe@pepe");
+mihash->insert("pepe@pepe","pepe@pepe");
+mihash->insert("pepe@pepe1","pepe@pepe1");
+cout << mihash->unicos();
+
+ // cout << mihash->buscar("pepe1") << endl;
     return 0;
 }
---
+++ tads/HashAbierto.cpp
@@ -0,0 +1,81 @@
+#include "ListImp.cpp"
+#include <iostream>
+using namespace std;
+
+template <typename k, typename v>
+struct keyValue
+{ // estructura de la tabla
+    k key;
+    v value;
+    keyValue(k key, v value)
+    {
+        this->key = key;
+        this->value = value;
+    }
+
+    bool operator==(const keyValue<k, v> &other) const
+    {
+        if (this->key == other.key && this->value == other.value)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    };
+};
+
+template <typename k, typename v>
+class HashAbierto
+{
+private:
+    ListImp<keyValue<k, v> > *table; // la tabla
+    int n;                          // cant elementos
+    int b;                          // tamaño de la tabla o cant buckets
+    int (*hash)(k);                 // funcion de hash
+    
+    float factorDeCarga()
+    {
+        return (float)this->n / this->b;
+    }
+
+public:
+    HashAbierto(int _b, int (*unhasg)(k))
+    {
+        table = new ListImp<keyValue<k, v> >[_b];
+        // table = new List<keyValue<k, v> >*[_b]();
+        b = _b;
+        n = 0;
+        hash = unhasg;
+    };
+
+    void insert(k key, v value)
+    {
+        if (factorDeCarga() > 0.7)
+        {
+            // hago rehash
+        }
+        int pos = abs(this->hash(key)) % this->b;
+        table[pos].insert(keyValue<k, v>(key, value));
+      /*  if (table[pos] == nullptr)
+        {
+            table[pos].insert(keyValue<k, v>(key, value));
+            cout << "Inserte " << key << endl;
+            n++;
+        }*/
+    };
+
+    int unicos (){
+        int mails_unicos = 0;
+        for (int i = 0; i < b; i++)
+        {
+            ListImp<keyValue<k, v> > &bucket = table[i];
+            if (bucket.getSize() == 1){
+            mails_unicos++;
+        }   
+        }
+        return mails_unicos;
+        
+    };
+};
