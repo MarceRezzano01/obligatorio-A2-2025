d7d8f8c4ced931bcfcea061b342fd29cd20b8bce
MarceloNicolas RezzanoDiaz <marcelo.rezzanodiaz@ukg.com>
2025-03-30 12:37:58 -0300

    ej 2 init

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -2,11 +2,82 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/HashCerrado.cpp"
+
 
 using namespace std;
 
+
+int hash3(string k){
+    int h = 0;
+    for (int i = 0; i < k.length(); i++)
+    h = 31 * h + int(k[i]);
+    return h;
+
+}
+
+int secondaryHashString(string key, int buckets) {
+    int hash = 0;
+    for (char c : key) {
+        hash = (hash * 17 + c) % 1000;
+    }
+    return 7 - (hash % 7);
+}
+
+int sencondHash(string palabra, int buckets)
+{
+    int pos = 0;
+    for (int i = 0; i <= 20; i++)
+    {
+        if (palabra[i] != '\0')
+        {
+            pos += palabra[i] * 32 ^ i;
+        }
+        else
+            return pos % buckets;
+    }
+    return pos % buckets;
+}
+
+
 int main()
 {
-    // TODO
+    int n_reads;
+    int m_exist;
+    cin >> n_reads;
+    HashCerrado<string,string> *mihash = new HashCerrado<string,string>(n_reads,hash3,secondaryHashString);
+
+
+  for (int i = 0; i < n_reads; i++)
+    {
+        string palabraInsert;
+        cin >> palabraInsert;
+        mihash->insert(palabraInsert,palabraInsert);
+    } 
+/*
+     cin >> m_exist;
+
+     for (int i = 0; i < m_exist; i++)
+    {
+        string palabraSearch;
+        cin >> palabraSearch;
+        cout << mihash->existe(palabraSearch) <<endl;
+    } 
+
+
+   //cout << "unicoss " << mihash->unicos();
+*/
+
+/*    
+HashCerrado<string,int> *mihash = new HashCerrado<string,int>(3,hash3,sencondHash);
+mihash->insert("pepe",0);
+mihash->insert("pepe1",1);
+mihash->insert("pepe2",2);
+
+  cout << mihash->existe("pepe1") << endl;
+  cout << mihash->existe("pepe4") << endl;
+
+*/
+
     return 0;
 }
---
+++ tads/HashCerrado.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+template <typename k, typename v>
+struct keyValue{//estructura de la tabla
+    k key;
+    v value;
+    keyValue(k key, v value){
+        this->key = key;
+        this->value = value;
+    }
+};
+
+
+
+template <typename k, typename v>
+class HashCerrado{
+  private:
+        keyValue<k, v> **table;//la tabla
+        int n;//cant elementos
+        int b;//tamaño de la tabla o cant buckets
+        int (*hash)(k); //funcion de hash
+        int (*SecondHash)(k,int);
+
+         float factorDeCarga() {
+            return (float)this->n/this->b;
+        }
+
+  public:
+        HashCerrado(int _b,int (*unhasg)(k),int (*secHash)(k,int)){
+            table = new keyValue<k,v>*[_b]();
+            for(int i=0; i < _b; i++) {
+                table[i] = nullptr;
+            }
+            b=_b;
+            n=0;
+            hash=unhasg;
+            SecondHash=secHash;
+        };
+
+
+        /// Doble hashing: intento i =(H(k)+h2(k)x i) % _b
+// h(x) fun hash y h2(x) otra funcion de hash
+
+         void insert(k key, v value){    
+            int pos = abs(this->hash(key))%this->b;
+            bool insert=false;
+            int intentos =0;
+            while (!insert){
+                if(table[pos]==nullptr){  
+                    table[pos]=new keyValue<k,v>(key,value);
+                    insert=true;
+                    cout<< "Inserte "<< key<< endl;
+                    n++;
+                }else if(table[pos]->key == key){
+                table[pos]->value=value;
+                insert=true;
+                }
+             if (!insert) {
+               intentos++; 
+               int hagoSecH = abs(this->SecondHash(key, this->b)%this->b);
+               cout << "SegHas " << hagoSecH << endl;
+               pos = (pos + (hagoSecH * intentos))%this->b;
+               cout << "PROXIMA POS A INSERT " << pos << endl;
+            }
+            }
+         };
+        
+        int existe(k key){
+        
+        int pos = abs(this->hash(key))%this->b;
+        int index = 0;
+         while (b>index){
+           if(table[pos] == nullptr) {
+                    return 0;
+           }else if (table[pos]->key==key){
+                 return 1;
+             }
+             index++;
+             pos=(pos+ (this->SecondHash(key,this->b))*index)%this->b;
+            
+         }
+         return 0;
+        }           
+
+};
