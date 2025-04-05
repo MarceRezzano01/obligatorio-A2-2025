99ee50218fba9f476a5ed2b6a0ed60eee3f1dbab
MarceloNicolas RezzanoDiaz <marcelo.rezzanodiaz@ukg.com>
2025-04-05 18:09:33 -0300

    ej 3 pasando todos los casos

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -12,33 +12,30 @@ using namespace std;
 int main()
 {
     int n_reads;
-   // cin >> n_reads;
+    cin >> n_reads;
 
 
-    MinHeap<int> *miHeap = new MinHeap<int>(4);
-    miHeap->insert(12);
-    miHeap->insert(10);
-    miHeap->insert(4);
-    miHeap->insert(6);
+    MinHeap<int> *miHeap = new MinHeap<int>(n_reads);
+   // miHeap->insert(12);
+   // miHeap->insert(10);
+   // miHeap->insert(4);
+   // miHeap->insert(6);
 
 
 
-     for (int i = 0; i < 4; i++)
+     for (int i = 0; i < n_reads; i++)
+    {
+        int numInsert;
+        cin >> numInsert;
+        miHeap->insert(numInsert);
+    } 
+
+
+    for (int i = 0; i < n_reads; i++)
     {
         cout << miHeap->retornoYeliminoTope()<<endl;
     } 
 
 
-/* 
-for (int i = 0; i < n_reads; i++)
-    {
-        string mail;
-        cin >> mail;
-        mihash->insert(mail,mail);
-    } 
-
-   cout << "unicoss " << mihash->unicos();
-
-   */
    return 0;
 }
