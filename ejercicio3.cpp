16d252bd5f900bd9336024c84c041309c128a8d8
dlachaisekz <dominique@kzsoftworks.com>
2025-04-09 22:28:13 -0300

    cambio el insert por el insert sin ordenar y llamo al heapify antes de desencolar los valores

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -4,38 +4,29 @@
 #include <limits>
 #include "tads/Heap.cpp"
 
-
 using namespace std;
 
-
-
-int main()
-{
+int main() {
     int n_reads;
     cin >> n_reads;
 
 
     MinHeap<int> *miHeap = new MinHeap<int>(n_reads);
-   // miHeap->insert(12);
-   // miHeap->insert(10);
-   // miHeap->insert(4);
-   // miHeap->insert(6);
-
 
 
      for (int i = 0; i < n_reads; i++)
     {
         int numInsert;
         cin >> numInsert;
-        miHeap->insert(numInsert);
-    } 
+        miHeap->insertSinOrdenar(numInsert);
+    }
 
+    miHeap->heapify();
 
-    for (int i = 0; i < n_reads; i++)
-    {
-        cout << miHeap->retornoYeliminoTope()<<endl;
-    } 
+    for(int i = 0; i < n_reads; i++) {
+        cout << miHeap->retornoYeliminoTope() << endl;
+    }
 
-
-   return 0;
+    delete miHeap;
+    return 0;
 }
