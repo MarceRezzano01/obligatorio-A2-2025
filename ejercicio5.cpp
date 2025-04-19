9fde67df2c860df3a1c52c157e3c09d699d24078
MarceloNicolas RezzanoDiaz <marcelo.rezzanodiaz@ukg.com>
2025-04-19 20:26:58 -0300

    ej 5 base case witouth ColaFifo import

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -2,11 +2,21 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/ListAdy.cpp"
 
 using namespace std;
 
 int main()
 {
-    // TODO
-    return 0;
+  ListAdy *miGrafito = new ListAdy(5);
+  miGrafito->aniadirArista(1, 2);
+  miGrafito->aniadirArista(2, 3);
+  miGrafito->aniadirArista(3, 4);
+  miGrafito->aniadirArista(4,2);
+    
+  bool result = hayCiclo(miGrafito);
+
+cout << "Hay ciclo: " << result;
+
+  return 0;
 }
---
+++ tads/ListAdy.cpp
@@ -0,0 +1,116 @@
+//#include "ColaFifo.cpp"
+#include <climits>
+#include <iostream>
+
+using namespace std;
+
+struct Arista {
+  int origen;
+  int destino;
+  int peso;
+  Arista() {}
+  Arista(int _origen, int _destino, int _peso = 1) {
+    origen = _origen;
+    destino = _destino;
+    peso = _peso;
+  }
+};
+
+template<typename T>
+struct Nodo{
+   T dato;
+   Nodo<T> *sig;
+   Nodo( T _dato, Nodo<T> *_sig){
+    dato = _dato;
+    sig = _sig;
+   } 
+   Nodo(T _dato) {
+    dato = _dato;
+    sig = nullptr;
+  }
+};
+
+class ListAdy {
+
+    private:
+
+        int cantV;
+        int A;
+        Nodo <Arista> **grafo;
+
+
+    public:
+
+     ListAdy(int _V){
+        cantV = _V;
+        A=0;
+        grafo = new Nodo<Arista>*[cantV+1]();
+        for (int i = 0; i <= cantV; i++)
+        {
+            grafo[i]=nullptr;
+        }
+    }
+
+     Nodo<Arista> *adyacentesA(int origen) {
+        Nodo<Arista> *aux = grafo[origen];
+        Nodo<Arista> *aristasClone = nullptr;
+        while (aux != nullptr) {
+            aristasClone = new Nodo<Arista>(aux->dato, aristasClone);
+            aux = aux->sig;
+        }
+     return aristasClone;
+  }
+
+   void aniadirArista(int origen, int destino, int peso = 1) {
+    assert(origen >= 1 && origen <= cantV);
+    assert(destino >= 1 && destino <= cantV);
+    Arista arista(origen, destino, peso);
+    Nodo<Arista> *nuevoNodo = new Nodo<Arista>(arista, grafo[origen]);
+    grafo[origen] = nuevoNodo;
+    A++;
+  }
+   
+   int getV() { return cantV; }
+
+  int getA() { return A; }
+};
+
+  int processNodes(int InitNode, int nodesVisiteds,ListAdy *grafo,  int *gradoDeEntrada) {
+     Nodo<Arista> *ady = grafo->adyacentesA(InitNode);
+     while (ady != nullptr) {
+              int destino = ady->dato.destino;
+              gradoDeEntrada[destino]--;
+              if (gradoDeEntrada[destino] == 0) {
+                    processNodes(destino, nodesVisiteds++ ,grafo,gradoDeEntrada);
+      }
+      ady = ady->sig;
+    }   
+     return nodesVisiteds;
+    }
+
+ bool hayCiclo(ListAdy *grafo) {
+    int V = grafo->getV();
+    int extraNodes=0;
+    int *gradoDeEntrada = new int[V + 1]();
+
+    // Calcular el grado de entrada de cada nodo
+    for (int v = 1; v < V + 1; v++) {
+        Nodo<Arista> *ady = grafo->adyacentesA(v);
+        while (ady != nullptr) {
+        int destino = ady->dato.destino;
+        gradoDeEntrada[destino]++;
+        ady = ady->sig;
+        }
+    }
+   //  QueueImp<int> *cola = new QueueImp<int>();
+     for (int v = 1; v < V + 1; v++) {
+      if (gradoDeEntrada[v] == 0) {
+        cout << v << endl;
+         extraNodes++;
+         extraNodes = processNodes(v,extraNodes,grafo,gradoDeEntrada);   
+    }
+  }
+    return extraNodes != V+1;
+}
+
+
