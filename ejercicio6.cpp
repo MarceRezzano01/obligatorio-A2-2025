4168d06284c422ff07084a1b22d6fe8004c0d11c
MarceloNicolas RezzanoDiaz <marcelo.rezzanodiaz@ukg.com>
2025-06-07 20:21:59 -0300

    ej 6 init

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -2,11 +2,27 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/EdificiosDac.cpp"
 
 using namespace std;
 
 int main()
 {
-    // TODO
+    //int cvert;
+    //cin >> cvert;
+    //int caristas;
+    //cin >> caristas;
+
+
+    EdificiosDac *edificios = new EdificiosDac(5);
+    edificios->agregarEdificio(2, 9, 10);
+    edificios->agregarEdificio(3,7,15);
+    edificios->agregarEdificio(5, 12, 12);
+    edificios->agregarEdificio(15, 20, 10);
+    edificios->agregarEdificio(19, 24, 8);
+
+
+
+
     return 0;
 }
---
+++ tads/EdificiosDac.cpp
@@ -0,0 +1,129 @@
+#include "ListImp.cpp"
+
+struct Edificio{
+    int posInicio;
+    int posFin;
+    int altura;
+    Edificio(int inicio, int fin, int alt){
+        this->posInicio = inicio;
+        this->posFin = fin;
+        this->altura = alt;
+    } 
+};
+
+struct Pares{
+    int inicio;
+    int altura;
+    Pares(int _inicio, int _altura){
+        this->inicio = _inicio;
+        this->altura = _altura;
+    }
+
+       bool operator==(const Pares &other) const
+    {
+        if (this->inicio == other.inicio && this->altura == other.altura)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    };
+};
+
+
+class EdificiosDac {
+
+ private:
+        Edificio ** SiuetasEdificios;
+        int n =1;
+        int initDac;
+        int endDac;
+        int cantEdif;
+        ListImp<Pares> *siluetasPegadas;
+        //Par **siluetasPegadas;
+
+
+  public:
+
+    EdificiosDac(int _cantEdificios) {
+            this->SiuetasEdificios = new Edificio*[_cantEdificios]();
+            for (int i = 0; i < _cantEdificios; i++) {
+                this->SiuetasEdificios[i] = nullptr;
+            }
+            this->siluetasPegadas = new ListImp<Pares>[_cantEdificios];
+        
+            this->cantEdif = _cantEdificios;
+        };
+
+    ListImp<Pares> * obtenerSilueta( Edificio ** SiuetasEdificios, int inicio, int fin) {
+        if (this->cantEdif == 1){
+           this->siluetasPegadas->insert(Pares(SiuetasEdificios[0]->posInicio, SiuetasEdificios[0]->altura));
+           return this->siluetasPegadas;
+        } 
+
+        //int mid = inicio + (fin - inicio) / 2;
+        int mid = cantEdif/ 2;
+         ListImp<Pares> * siluetaIzq = obtenerSilueta(SiuetasEdificios, inicio, mid);
+         ListImp<Pares> * SiluestaDer = obtenerSilueta(SiuetasEdificios, mid + 1, fin);
+
+        return mergeSiluetas(siluetaIzq, SiluestaDer);
+       // return obtenerSilueta (SiuetasEdificios, 0, cantEdif - 1);
+      }
+
+    ListImp<Pares>* mergeSiluetas( ListImp<Pares>* left,  ListImp<Pares>*  right) {
+        ListImp<Pares> *merged= new ListImp<Pares>[this->cantEdif];
+        int h1 = 0, h2 = 0, i = 0, j = 0;
+
+        while (i < left->getSize() && j < right->getSize()) {
+            if (left->get(i).inicio < right->get(j).inicio) {
+                int x = left->get(i).inicio;
+                h1 = left->get(i).altura;
+                int maxH = max(h1, h2);
+                merged->insert(Pares(x, maxH));
+                i++;
+            } else if (left->get(i).inicio  > right->get(j).inicio) {
+                int x = right->get(j).inicio;
+                h2 = right->get(j).altura;
+                int maxH = max(h1, h2);
+                merged->insert(Pares(x, maxH));
+                j++;
+            } else {
+                int x = left->get(i).inicio;
+                h1 =  left->get(i).altura;
+                h2 = right->get(j).altura;
+                int maxH = max(h1, h2);
+                merged->insert(Pares(x, maxH));
+                i++;
+                j++;
+            }
+        }
+
+        while (i < left->getSize()) {
+            merged->insert(Pares(left->get(i).inicio, left->get(i).altura));
+            i++;
+        }
+
+        while (j < right->getSize()) {
+             merged->insert(Pares( right->get(j).inicio,  right->get(j).altura));
+            j++;
+        }
+
+        return merged;
+    }
+
+
+   
+    void agregarEdificio(int inicio, int fin, int altura) {
+        this->SiuetasEdificios[n] =  new Edificio(inicio, fin, altura);
+        
+    }
+    
+    int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+
+
+};
