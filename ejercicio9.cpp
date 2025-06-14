ed17df9d4139c9161d1f81f6f8156feaa8442a34
MarceloNicolas RezzanoDiaz <marcelo.rezzanodiaz@ukg.com>
2025-06-14 16:27:54 -0300

    ej 9 pasando todos los casos

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -7,7 +7,34 @@
 using namespace std;
 
 int main()
+
 {
+
+    int n_archs;
+    int max_lienas;
+    int max_peso;
+    cin >> n_archs;
+    cin >> max_peso;
+    cin >> max_lienas;
+    UltimoMomentoTab *ultimoMomento = new UltimoMomentoTab(n_archs, max_peso, max_lienas);
+
+    for (int i = 0; i < n_archs; i++)
+    {
+        int tam_arch;
+        int cant_lineas_arch;
+        int puntos_ejer;
+
+        cin >> tam_arch;
+        cin >> cant_lineas_arch;
+        cin >> puntos_ejer;
+
+        ultimoMomento->agregarEjercicio(tam_arch, cant_lineas_arch, puntos_ejer);
+    }
+
+           cout << ultimoMomento->puntajeMax() << endl;
+
+           
+/*
     UltimoMomentoTab *ultimoMomento = new UltimoMomentoTab(10, 50, 1000);
     ultimoMomento->agregarEjercicio(16,665,1);
     ultimoMomento->agregarEjercicio(6,501,8);
@@ -19,9 +46,12 @@ int main()
     ultimoMomento->agregarEjercicio(22,565,7);
     ultimoMomento->agregarEjercicio(31,414,9);
     ultimoMomento->agregarEjercicio(49,389,2);
-    
-//SALIDA -> 17
-   cout << ultimoMomento->puntajeMax() << endl;
+
+       cout << ultimoMomento->puntajeMax() << endl;
+*/ 
+
+
+
   
     // TODO 
     return 0;
